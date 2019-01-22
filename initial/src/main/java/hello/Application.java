323cce4b6fbfaf1a2f00b7331d4fb4f7ae69f9db
Volkan Yalçın <volkanyalcin@Volkans-MacBook-Pro.local>
2019-01-22 13:58:27 +0300

    2nd update

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World-some changes";
+		return "Hello Docker World-some changes. 2nd update";
 	}
 
 
